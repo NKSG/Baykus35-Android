f12647a5e351ca8a778c4670eb0a00a8e0333791
Onur Ersen <onurersen@gmail.com>
2013-11-08 13:57:12 +0200

    Splash screen added.Implemented.

--- Baykus35/gen/com/onurersen/baykus35/R.java
+++ Baykus35/gen/com/onurersen/baykus35/R.java
@@ -26,6 +26,7 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int ic_list_bus=0x7f020001;
+        public static final int splash=0x7f020002;
     }
     public static final class id {
         public static final int container=0x7f080001;
@@ -47,6 +48,7 @@ public final class R {
         public static final int fragment_tariff=0x7f030004;
         public static final int row_route=0x7f030005;
         public static final int row_tariff=0x7f030006;
+        public static final int splash=0x7f030007;
     }
     public static final class string {
         public static final int app_name=0x7f060000;
