9d762e8167951a5463587232fe4abcfc41b6b6b4
Onur Ersen <onurersen@gmail.com>
2013-11-08 11:36:31 +0200

    CustomTextView and TypeFaceSpan classes added.

---
+++ Baykus35/src/com/onurersen/baykus35/customviews/TypeFaceSpan.java
@@ -0,0 +1,42 @@
+package com.onurersen.baykus35.customviews;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.text.TextPaint;
+import android.text.style.MetricAffectingSpan;
+import android.util.LruCache;
+
+/**
+ * 
+ * @author onurersen
+ * 
+ */
+public class TypeFaceSpan extends MetricAffectingSpan {
+
+	private static LruCache<String, Typeface> sTypefaceCache = new LruCache<String, Typeface>(12);
+
+	private Typeface mTypeface;
+
+	public TypeFaceSpan(Context context, String typefaceName) {
+		mTypeface = sTypefaceCache.get(typefaceName);
+
+		if (mTypeface == null) {
+			mTypeface = Typeface.createFromAsset(context.getApplicationContext().getAssets(),
+					String.format("fonts/%s.ttf", typefaceName));
+			sTypefaceCache.put(typefaceName, mTypeface);
+		}
+	}
+
+	@Override
+	public void updateMeasureState(TextPaint p) {
+		p.setTypeface(mTypeface);
+		p.setFlags(p.getFlags() | Paint.SUBPIXEL_TEXT_FLAG);
+	}
+
+	@Override
+	public void updateDrawState(TextPaint tp) {
+		tp.setTypeface(mTypeface);
+		tp.setFlags(tp.getFlags() | Paint.SUBPIXEL_TEXT_FLAG);
+	}
+}
