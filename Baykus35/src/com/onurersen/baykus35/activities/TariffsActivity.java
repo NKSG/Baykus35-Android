7aaef3347c62cb51786e30bc56f79188b65bf868
Onur Ersen <onurersen@gmail.com>
2013-11-13 09:02:41 +0200

    Maps get working.

--- Baykus35/src/com/onurersen/baykus35/activities/TariffsActivity.java
+++ Baykus35/src/com/onurersen/baykus35/activities/TariffsActivity.java
@@ -9,13 +9,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.onurersen.baykus35.R;
 import com.onurersen.baykus35.customization.CustomArrayAdapter;
 import com.onurersen.baykus35.db.sql.SQLiteDatabaseHelper;
 import com.onurersen.baykus35.list.tariff.TariffItemAdapter;
 import com.onurersen.baykus35.list.tariff.TariffModel;
+import com.onurersen.baykus35.utility.AlertUtil;
+import com.onurersen.baykus35.utility.LogCat;
 
 /**
  * 
@@ -26,6 +32,10 @@ public class TariffsActivity extends FragmentActivity implements ActionBar.OnNav
 
 	private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
 	private int selectedRouteId = -1;
+
+	private int DROPDOWN_INDEX_TARIFF = 0;
+	private int DROPDOWN_INDEX_MAP = 1;
+	private int DROPDOWN_INDEX_ABOUT = 2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -59,12 +69,28 @@ public class TariffsActivity extends FragmentActivity implements ActionBar.OnNav
 
 	@Override
 	public boolean onNavigationItemSelected(int position, long id) {
-		Fragment fragment = new TariffSectionFragment();
-		Bundle args = new Bundle();
-		args.putInt(TariffSectionFragment.ARG_SECTION_NUMBER, position + 1);
-		args.putInt(TariffSectionFragment.ARG_SELECTED_ROUTE_ID, getSelectedRouteId());
-		fragment.setArguments(args);
-		getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+		if (position == DROPDOWN_INDEX_TARIFF) {
+			Fragment fragment = new TariffSectionFragment();
+			Bundle args = new Bundle();
+			args.putInt(TariffSectionFragment.ARG_SECTION_NUMBER, position + 1);
+			args.putInt(TariffSectionFragment.ARG_SELECTED_ROUTE_ID, getSelectedRouteId());
+			fragment.setArguments(args);
+			getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+		} else if (position == DROPDOWN_INDEX_MAP) {
+			Fragment fragment = new MapSectionFragment();
+			Bundle args = new Bundle();
+			args.putInt(TariffSectionFragment.ARG_SECTION_NUMBER, position + 1);
+
+			fragment.setArguments(args);
+			getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+		} else if (position == DROPDOWN_INDEX_ABOUT) {
+			Fragment fragment = new AboutSectionFragment();
+			Bundle args = new Bundle();
+			args.putInt(TariffSectionFragment.ARG_SECTION_NUMBER, position + 1);
+
+			fragment.setArguments(args);
+			getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+		}
 		return true;
 	}
 
@@ -105,17 +131,97 @@ public class TariffsActivity extends FragmentActivity implements ActionBar.OnNav
 		 * fragment.
 		 */
 		public static final String ARG_SECTION_NUMBER = "section_number";
+		SupportMapFragment fm;
+		GoogleMap map;
+		private static View rootView;
+
+		final LatLng CENTER = new LatLng(43.661049, -79.400917);
+
+		class Data {
+			public Data(float lng, float lat, String title, String snippet) {
+				super();
+				this.lat = (float) lat;
+				this.lng = (float) lng;
+				this.title = title;
+				this.snippet = snippet;
+			}
+
+			float lat;
+			float lng;
+			String title;
+			String snippet;
+		}
+
+		Data[] data = {
+				new Data(-79.400917f, 43.661049f, "New New College Res", "Residence building (new concrete high-rise)"),
+				new Data(-79.394524f, 43.655796f, "Baldwin Street", "Here be many good restaurants!"),
+				new Data(-79.402206f, 43.657688f, "College St",
+						"Many discount computer stores if you forgot a cable or need to buy hardware."),
+				new Data(-79.390381f, 43.659878f, "Queens Park Subway",
+						"Quickest way to the north-south (Yonge-University-Spadina) subway/metro line"),
+				new Data(-79.403732f, 43.666801f, "Spadina Subway",
+						"Quickest way to the east-west (Bloor-Danforth) subway/metro line"),
+				new Data(-79.399696f, 43.667873f, "St George Subway back door",
+						"Token-only admittance, else use Spadina or Bedford entrances!"),
+				new Data(-79.384163f, 43.655083f, "Eaton Centre (megamall)",
+						"One of the largest indoor shopping centres in eastern Canada. Runs from Dundas to Queen."), };
 
 		public MapSectionFragment() {
 		}
 
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_map, container, false);
-			TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
-			dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+
+			if (rootView != null) {
+				ViewGroup parent = (ViewGroup) rootView.getParent();
+				if (parent != null)
+					parent.removeView(rootView);
+			}
+			try {
+				if (rootView == null)
+					rootView = inflater.inflate(R.layout.fragment_map, container, false);
+			} catch (Exception exception) {
+				LogCat.INSTANCE.error(this.getClass().getName(), exception.getMessage());
+				// AlertUtil.messageAlert(getActivity().getApplicationContext(),
+				// getActivity().getString(R.string.map_error_title),
+				// exception.getMessage());
+			}
+
+			for (Data d : data) {
+				LatLng location = new LatLng(d.lat, d.lng);
+				getMap().addMarker(new MarkerOptions().position(location).title(d.title).snippet(d.snippet));
+			}
+
+			getMap().setIndoorEnabled(true);
+			// getMap().setMyLocationEnabled(true);
+			getMap().moveCamera(CameraUpdateFactory.zoomTo(14));
+			getMap().animateCamera(CameraUpdateFactory.newLatLng(CENTER), 1750, null);
+
 			return rootView;
 		}
+
+		public SupportMapFragment getFm() {
+			if (fm == null) {
+				fm = (SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.map);
+			}
+			return fm;
+		}
+
+		public void setFm(SupportMapFragment fm) {
+			this.fm = fm;
+		}
+
+		public GoogleMap getMap() {
+			if (map == null) {
+				map = getFm().getMap();
+			}
+			return map;
+		}
+
+		public void setMap(GoogleMap map) {
+			this.map = map;
+		}
+
 	}
 
 	public static class AboutSectionFragment extends Fragment {
@@ -131,8 +237,6 @@ public class TariffsActivity extends FragmentActivity implements ActionBar.OnNav
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_about, container, false);
-			TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
-			dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
 			return rootView;
 		}
 	}
